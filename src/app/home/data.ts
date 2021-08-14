944384fe7e7f96f94122f15e757efe4faba37171
HayrambanTJ <78908193+HayrambanTJ@users.noreply.github.com>
2021-08-14 14:08:23 +0530

    Update data.ts

--- src/app/home/data.ts
+++ src/app/home/data.ts
@@ -5,10 +5,10 @@ const data = {
 	"description": "I am obsessed with two things: coffee and the web. Luckily being in Developer Relations, I get my fill of both every single day- whether through building new applications, working on tools for developers, or sharing my experiences across the world!No amount of time is ever enough to do everything I want. So I have to stay moving by writing guides, building cool apps, teaching fledgling developers the voodoo magic of the web and most importantly Drinking coffee...",
 	"experiences": [
 		{
-			"title": "Developer Program Engineer",
+			"title": "PHP Developer & Angular Developer",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-0.webp",
-			"institution": "Google",
-			"start": "2019-11-01T07:00:00.000Z",
+			"institution": "Abaxys Tech Private Limited",
+			"start": "2021-03-15T07:00:00.000Z",
 			"end": null,
 			"current": true,
 			"description": "Serve as as a liaison between Developer Advocacy, Technical Writing, Product Management, and Engineering. Actively connect with developers and speak externally about technologies on conference panels, at meet ups, and on blogs. Engage in the open source community in order to better advocate for the community’s needs. Promote the adoption of modern web technologies. Develop and maintain web.dev, Google's platform for guidance and analysis for web developers.",
@@ -16,20 +16,20 @@ const data = {
 			"type": "work"
 		},
 		{
-			"title": "Google Developer Expert",
+			"title": "PHP Developer",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/volunteer-0.webp",
-			"institution": "Google Developers",
-			"start": "2019-05-01T07:00:00.000Z",
-			"end": "2019-11-01T07:00:00.000Z",
+			"institution": "PHP SCRIPT DIRECTORY",
+			"start": "2018-05-01T07:00:00.000Z",
+			"end": "2020-12-19T07:00:00.000Z",
 			"current": false,
 			"description": "Recognized by Google as having exemplary expertise in one or more of their Google Developers products. Actively supported developers, companies and tech communities by speaking at events, publishing content.",
 			"icon": "group",
 			"type": "volunteer"
 		},
 		{
-			"title": "GDG Organizer",
+			"title": "ANGULAR COURSE",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/volunteer-1.webp",
-			"institution": "Google Developers",
+			"institution": "UDEMY",
 			"start": "2016-01-01T08:00:00.000Z",
 			"end": "2019-11-01T07:00:00.000Z",
 			"current": false,
@@ -38,43 +38,21 @@ const data = {
 			"type": "volunteer"
 		},
 		{
-			"title": "Developer Advocate",
+			"title": "BACKEND DEVELOPER",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-1.webp",
-			"institution": "Typeform.com",
-			"start": "2019-01-01T08:00:00.000Z",
-			"end": "2019-10-01T07:00:00.000Z",
+			"institution": "PHP Script Directory",
+			"start": "2018-11-12T08:00:00.000Z",
+			"end": "2020-12-19T07:00:00.000Z",
 			"current": false,
 			"description": "Promote the adoption of Typeform while collecting constructive feedback to make the platform better. Built tools for developers to easily use Typeform services and APIs while also building examples and writing guides of how to use Typeform. Explored and developed integrations with other platforms.",
 			"icon": "business",
 			"type": "work"
 		},
 		{
-			"title": "Front End Engineer",
-			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-2.webp",
-			"institution": "Sellpoints, Inc.",
-			"start": "2018-05-01T07:00:00.000Z",
-			"end": "2019-01-01T08:00:00.000Z",
-			"current": false,
-			"description": "Revamped UI, UX and interface for client-facing retargeting platform as well as maintained other e-Commerce marketing platforms. Implemented modern web development techniques, such as custom elements, into older frameworks and software in order to migrate them into new best practices.",
-			"icon": "business",
-			"type": "work"
-		},
-		{
-			"title": "Front End Engineer",
-			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-3.webp",
-			"institution": "Checkmate.Digital",
-			"start": "2015-10-01T07:00:00.000Z",
-			"end": "2018-05-01T07:00:00.000Z",
-			"current": false,
-			"description": "Lead development tasks within a fullstack scope such as creating REST API components, developing native applications for consumption of APIs, and cultivating user-friendly training programs. Provide continuous improvement of open source projects. Aggressively source and hire the majority of development staff including the current CTO.",
-			"icon": "business",
-			"type": "work"
-		},
-		{
-			"title": "Bachelor of Science (B.S.)",
+			"title": "B.E Computer Science Engineering",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/education-0.webp",
-			"institution": "Southern Connecticut State University",
-			"start": "2012-01-01T00:00:00.000Z",
+			"institution": "Panimalar Engineering Colledge",
+			"start": "2014-01-01T00:00:00.000Z",
 			"end": "2018-01-01T00:00:00.000Z",
 			"current": false,
 			"description": "Computer Science",
@@ -82,49 +60,28 @@ const data = {
 			"type": "education"
 		},
 		{
-			"title": "Lead Developer",
+			"title": "12th",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-4.webp",
-			"institution": "Kricket App",
-			"start": "2017-01-01T08:00:00.000Z",
-			"end": "2017-11-01T07:00:00.000Z",
+			"institution": "DAV MATRICULATION HIGHER SECONDARY SCHOOL",
+			"start": "2013-01-01T08:00:00.000Z",
+			"end": "2014-11-01T07:00:00.000Z",
 			"current": false,
 			"description": "Collaboratively developed complete rework of the Kricket application which included creation of a database relational model, REST API endpoints, and native mobile applications. Networked with clients to design custom solutions for integration of geodata. Presented at multiple conferences on processes and methodologies of utilizing client side geodata.",
-			"icon": "business",
-			"type": "work"
+			"icon": "school",
+			"type": "education"
 		},
 		{
-			"title": "Product Forum Top Contributor",
+			"title": "10th",
 			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/volunteer-2.webp",
-			"institution": "Google",
+			"institution": "DAV Matric Higher Secondary School",
 			"start": "2011-06-01T07:00:00.000Z",
-			"end": "2016-12-01T08:00:00.000Z",
+			"end": "2012-12-01T08:00:00.000Z",
 			"current": false,
 			"description": "Actively participate in Google’s Product Help Forums. Work with the Mobile team of Google Consumer Operations branch while addressing consumer questions, providing support for Google’s various mobile products and services, and reporting/escalating bugs with the Android platform, Nexus devices, and some Google services available on Android.",
-			"icon": "group",
-			"type": "volunteer"
-		},
-		{
-			"title": "Senior Apprentice",
-			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/volunteer-3.webp",
-			"institution": "Independent Software",
-			"start": "2015-09-01T07:00:00.000Z",
-			"end": "2016-12-01T08:00:00.000Z",
-			"current": false,
-			"description": "Organize weekly meetings and hack nights, support software tutoring sessions for all levels of expertise, and serve as an apprentice within the Apprentice 100 program.",
-			"icon": "group",
-			"type": "volunteer"
-		},
-		{
-			"title": "A100 Program Apprentice",
-			"src": "https://storage.googleapis.com/michaelsolati-com.appspot.com/home/work-5.webp",
-			"institution": "Independent Software",
-			"start": "2015-06-01T07:00:00.000Z",
-			"end": "2015-09-01T07:00:00.000Z",
-			"current": false,
-			"description": "Functionally led team as a Scrum Master while leveraging knowledge growth/training in categories of modern coding technologies and agile development. Utilized a wide toolset for software development projects and tasks.",
-			"icon": "business",
-			"type": "work"
+			"icon": "school",
+			"type": "education"
 		}
+		
 	],
 	"image": "./assets/myphoto.jpg"
 };
